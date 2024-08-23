20f4d7008256ac5e03a02734e0ff11711584a324
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-23 19:34:43 +0200

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,10 +1,5 @@
 import streamlit.components.v1 as components
 import streamlit as st
 
-st.html(
-    "<iframe
-  src="https://30days.streamlit.app?embed=true"
-  style="height: 450px; width: 100%;"
-></iframe>"
-)
+st.html("<iframe  src="https://30days.streamlit.app?embed=true"  style="height: 450px; width: 100%;"></iframe>")
 
