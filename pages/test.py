f140e404d4a66b05efac404c2ce0ee6a92543bb9
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-23 18:46:11 +0200

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,3 +1,3 @@
 import streamlit as st
-<iframe src="https://rainfall-prediction-app-volkan-ai.streamlit.app/"
+<iframe src="https://rainfall-prediction-app-volkan-ai.streamlit.app/?embed=true"
   style="height: 450px; width: 100%;"></iframe>
