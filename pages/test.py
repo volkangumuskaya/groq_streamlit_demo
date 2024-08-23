6b583c3c245f347dcbfde44e922693da27e0b76d
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-23 18:47:58 +0200

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,3 +1,17 @@
 import streamlit as st
+
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title='volkan-ai',
+    layout="wide",
+    page_icon=':volcano:', # This is an emoji shortcode. Could be a URL too.
+)
+
+# -----------------------------------------------------------------------------
+# Draw the actual page
+
+# Set the title that appears at the top of the page.
+st.image('images/el-chalten.jpg','El Chalten, Patagonia')
+
 <iframe src="https://rainfall-prediction-app-volkan-ai.streamlit.app/?embed=true"
   style="height: 450px; width: 100%;"></iframe>
