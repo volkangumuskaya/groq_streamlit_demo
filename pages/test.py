5cbeb1132fe74d01ddce4a226fb03b5e5ff35c4b
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-23 18:37:06 +0200

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,4 +1,4 @@
 <iframe
-  src="https://30days.streamlit.app?embed=true"
+  src="https://rainfall-prediction-app-volkan-ai.streamlit.app/?embed=true"
   style="height: 450px; width: 100%;"
 ></iframe>
