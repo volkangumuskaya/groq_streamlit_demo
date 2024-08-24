96762bddee1383e066ecfdcf388cc28b771a973f
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-24 13:34:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def icon(emoji: str):
 icon(":volcano:")
 
 st.subheader("Groq Chat Streamlit App", divider="rainbow", anchor=False)
-st.subheader(os.environ['GROQ_KEY'])
+# st.subheader(os.environ['GROQ_KEY'])
 st.subheader(os.getenv('GROQ_KEY'))
 st.subheader(os.getenv(GROQ_KEY))
 # client = Groq(
