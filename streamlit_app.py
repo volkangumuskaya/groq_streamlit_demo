fbb18fe62dd74849bbd14f71e62e5b9418fc5c5d
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-24 17:04:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def icon(emoji: str):
 
 # icon(":volcano:")
 
-st.image('images/el-chalten.jpg','El Chalten, Patagonia',width=120)
+st.image('images/el-chalten.jpg','El Chalten, Patagonia',width=600)
 
 st.subheader("Groq Chat Streamlit App", divider="rainbow", anchor=False)
 
