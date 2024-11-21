34d6e5d61f5b5e705b435b8b2a0fd49f59cd71f3
Volkan Gumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-11-21 15:19:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,21 +6,6 @@ GROQ_API_KEY=st.secrets["GROQ_API_KEY"]
 st.set_page_config(page_icon=":volcano:", layout="wide",
                    page_title="volkan-ai-chatbot")
 
-
-tmp_names = ['1. Initiate a plan', '2. Modify a plan', '3. Get insights', '4. Get results']
-nl = '\n'
-module_names = f"{nl}{nl.join(tmp_names)}"
-
-# System prompt (not displayed to the user)
-SYSTEM_PROMPT = f'''You are a specific AI assistant that try to understand what user wants and will invoke a number of modules. 
-If you find a reasonable match, I want you to respond in the format: The module match is: [Module name]. 
-You cannot respond with anything else. This is a strict requirement.
-The module names are as follows: \n
-{module_names}
-'''
-
-
-
 def icon(emoji: str):
     """Shows an emoji as a Notion-style page icon."""
     st.write(
@@ -65,19 +50,9 @@ client = Groq(
     api_key=st.secrets["GROQ_API_KEY"],
 )
 
-# Initialize chat history and system prompt
+# Initialize chat history and selected model
 if "messages" not in st.session_state:
-    st.session_state.messages = []  # Start with an empty chat history
-    st.session_state.welcome_message_shown = False
-
-
-# Display the welcome message only once
-if not st.session_state.welcome_message_shown:
-    with st.chat_message("assistant", avatar="🤖"):
-        st.markdown(f"""Hi, please explain what you want to do. Currently the main modules are: \n
-        {module_names}""")
-    st.session_state.welcome_message_shown = True
-
+    st.session_state.messages = []
 
 if "selected_model" not in st.session_state:
     st.session_state.selected_model = None
@@ -92,48 +67,72 @@ headers = {
 
 response = requests.get(url, headers=headers)
 
+# # Define model details once
+# if 'models' not in globals():
+#   models={}
+#   for x in response.json()['data']:
+#       try:
+#           chat_completion = client.chat.completions.create(
+#               messages=[
+#                   {
+#                       "role": "user",
+#                       "content": "Hey",
+#                   }
+#               ],
+#               model=x['id'],
+#           )
+#           models[x['id']] = {'name': x['id'], 'tokens': x['context_window'], 'developer': x['owned_by']}
+#       except:
+#           print(x['id'], 'not supported')
+
 import pickle
 if 'models' not in globals():
     with open('models_dict.pickle', 'rb') as handle:
       models = pickle.load(handle)
 
-# # Layout for model selection and max_tokens slider
+# Layout for model selection and max_tokens slider
 col1, col2 = st.columns(2)
+
+with col1:
+    model_option = st.selectbox(
+        "Choose a model:",
+        options=list(models.keys()),
+        format_func=lambda x: models[x]["name"],
+        index=5  # Default to mixtral
+    )
+
 # with col1:
 #     model_option = st.selectbox(
 #         "Choose a model:",
-#         options=list(models.keys()),
-#         format_func=lambda x: models[x]["name"],
-#         index=5  # Default to mixtral
+#         options=list(all_groq_supported_models),
+#         # format_func=lambda x: models[x]["name"],
+#         index=0  # Default to mixtral
 #     )
-# max_tokens_range = min(models[model_option]["tokens"],8000)
-
-# with col2:
-#     # Adjust max_tokens slider dynamically based on the selected model
-#     max_tokens = st.slider(
-#         "Max Tokens:",
-#         min_value=512,  # Minimum value to allow some flexibility
-#         max_value=max_tokens_range,
-#         # Default value or max allowed if less
-#         value=max(1024,max_tokens_range),
-#         step=512,
-#         help=f"Adjust the maximum number of tokens (words) for the model's response. Max for selected model: {max_tokens_range}"
-#     )
-model_option='llama3-70b-8192'
-max_tokens=1024
 
 # Detect model change and clear chat history if model has changed
 if st.session_state.selected_model != model_option:
-    st.session_state.messages = []  # Clear chat history
-    st.session_state.welcome_message_shown = False  # Reset welcome message
+    st.session_state.messages = []
     st.session_state.selected_model = model_option
+
+max_tokens_range = min(models[model_option]["tokens"],8000)
+
+with col2:
+    # Adjust max_tokens slider dynamically based on the selected model
+    max_tokens = st.slider(
+        "Max Tokens:",
+        min_value=512,  # Minimum value to allow some flexibility
+        max_value=max_tokens_range,
+        # Default value or max allowed if less
+        value=max(1024,max_tokens_range),
+        step=512,
+        help=f"Adjust the maximum number of tokens (words) for the model's response. Max for selected model: {max_tokens_range}"
+    )
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     avatar = '🤖' if message["role"] == "assistant" else '👨‍💻'
     with st.chat_message(message["role"], avatar=avatar):
         st.markdown(message["content"])
-      
 
 
 def generate_chat_responses(chat_completion) -> Generator[str, None, None]:
@@ -154,8 +153,12 @@ if prompt := st.chat_input("Enter your prompt here..."):
         chat_completion = client.chat.completions.create(
             model=model_option,
             messages=[
-                {"role": "system", "content": SYSTEM_PROMPT}
-            ] + st.session_state.messages,  # Add system prompt to API context
+                {
+                    "role": m["role"],
+                    "content": m["content"]
+                }
+                for m in st.session_state.messages
+            ],
             max_tokens=max_tokens,
             stream=True
         )
@@ -176,5 +179,3 @@ if prompt := st.chat_input("Enter your prompt here..."):
         combined_response = "\n".join(str(item) for item in full_response)
         st.session_state.messages.append(
             {"role": "assistant", "content": combined_response})
-    
-
